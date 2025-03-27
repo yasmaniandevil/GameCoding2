bb747f6013bd97bc95c6e3fc4c0b6344f9b5ec2c
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2025-03-26 23:16:43 -0400

    raycast.....

--- Game Coding 2 Projects/Assets/Disco2/OnMouse.cs
+++ Game Coding 2 Projects/Assets/Disco2/OnMouse.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class OnMouse : MonoBehaviour
 {
+    LayerMask mask;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mask = LayerMask.NameToLayer("Click");
     }
 
     // Update is called once per frame
@@ -19,5 +21,21 @@ public class OnMouse : MonoBehaviour
     private void OnMouseDown()
     {
         Debug.Log("mouse is down");
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition); //contrust a ray from current mouse cordinates
+        //Raycast hit;
+        //Raycast hit;
+        /*if (Physics.Raycast(ray, hit))
+        {
+            if(hit.transform.gameObject.layer == mask)
+            {
+                Debug.Log("NPC");
+            }
+            else
+            {
+                Debug.Log("No Layer");
+            }
+        }*/
+
+
     }
 }
