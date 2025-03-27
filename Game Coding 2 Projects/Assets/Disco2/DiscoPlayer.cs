b4e93e9fcf78ad69aac6984bbfa7082aa62fa0ec
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2025-03-27 14:23:09 -0400

    save

--- Game Coding 2 Projects/Assets/Disco2/DiscoPlayer.cs
+++ Game Coding 2 Projects/Assets/Disco2/DiscoPlayer.cs
@@ -7,7 +7,11 @@ public class DiscoPlayer : MonoBehaviour
     CharacterController cc;
     public float speed = 5f;
 
+    //store references to functions that take no parameters and return nothing
+    //placeholder for function //custom function type that we define ourselves
     public delegate void InputEvents();
+    //static var to act like global events
+    //empty until another script subscribes functions to them
     public static InputEvents OnInteract;
     public static InputEvents OnEnterInteractable;
     public static InputEvents OnExitInteractable;
@@ -29,6 +33,8 @@ public class DiscoPlayer : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("entered trigger");
+        //when onEnterInteractable happens i want you to also run my SHOWUI function
+        //if anything is subscribed to onEnterInteractable call all of them (whoever is listening)
         if (OnEnterInteractable != null) OnEnterInteractable();
     }
 
